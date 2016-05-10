a6c02d35f4500fa84dd09454a0ebcf10493d9915
Daniel Martinez <dmartinez@datacrm.com>
2016-05-10 17:35:52 -0500

    start

---
+++ conocimientoApp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils.encoding import uri_to_iri
+
+class Usuario(models.Model):
+    fecha_nacimiento = models.DateField()
+    nombres = models.CharField(max_length=50)
+    correo = models.EmailField()
+	peso = models.DecimalField(max_digits=5, decimal_places=2))
+    estatura= models.IntegerField();
+    ejercicio = models.IntegerField();
+    genero = models.CharField(max_length=1)
+    contrasena = models.CharField(max_length=30)
+    def __unicode__(self):
+        return unicode(self.email)
